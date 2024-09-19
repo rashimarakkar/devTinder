9a795773dadb1cd3620cb8375ae0712de5176381
Rashid <rashimarakkar@gmail.com>
2024-09-19 15:05:11 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const PORT = 3000;
 const app = express();
-const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.use("/admin", adminAuth);
 
-app.post("/user/login", (req, res) => {
-  res.send("user logged in successfully");
-});
 
-app.get("/user", userAuth, (req, res) => {
-  res.send("User data sent");
-});
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("all data sent");
-});
+app.get("/user", (req,res,next)=>{
 
-app.delete("/admin/getAllData", (req, res) => {
-  res.send("delete a user");
-});
+throw new Error("my created error")
+
+res.send("user data")
+
+})
+
+app.use("/", (err,req,res,next)=>{  
+
+    if(err){
+        res.status(401).send(err.message)
+    }
+})
 
 app.listen(PORT, () => {
   console.log(`Server started running successfully on port ${PORT}`);
