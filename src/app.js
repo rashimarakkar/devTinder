f7bfa820c3d118f42ba99fac84458ee149db2c8f
Rashid <rashimarakkar@gmail.com>
2024-09-19 13:27:21 +0530

    handling multiple route handler in express

--- src/app.js
+++ src/app.js
@@ -3,13 +3,27 @@ const PORT = 3000;
 
 const app = express();
 
+app.use("/users",(req,res,next)=>{
 
-app.get("/user/:id/:name/:age", (req, res) => {
-  
-    console.log(req.params);
+console.log('inside first route handler');
+
+next()
+// res.send("sending from first route handler")
+},
+(req,res,next)=>{
+
+console.log('inside second route handler');
+
+// res.send("sending from second route handler")
+next()
+},
+(req,res)=>{
+
+    console.log('inside third rout handler');
     
-  res.send(req.params);
-});
+    res.send("sending from third route handler")
+}
+)
 
 app.listen(PORT, () => {
   console.log(`Server started running successfully on port ${PORT}`);
