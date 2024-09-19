22dded4698b03bb12f9bf0b6b6ae36a97306eac2
Rashid <rashimarakkar@gmail.com>
2024-09-19 10:37:38 +0530

    created a web server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const PORT = 3000;
+
+const app = express();
+
+app.use("/test",(req,res)=>{
+    res.send('hellow from server');
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started running successfully on port ${PORT}`);
+});
