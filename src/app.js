e4b315d4011a32709114bf0cf070ae39d2d58cf7
Rashid <rashimarakkar@gmail.com>
2024-09-19 21:14:35 +0530

    get APIS

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.post("/signUp", async (req, res) => {
     console.log(req.body);
     
-  const user = new User({...req.body});
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User added successfully");
@@ -17,6 +17,40 @@ app.post("/signUp", async (req, res) => {
     res.status(400).send(error.message);
   }
 });
+
+app.post("/user", async (req,res)=>{
+  try {
+    const user = await User.findOne({email: req.body.email});
+
+    if(!user)  res.status(404).send(error.message)
+
+    res.send(user);
+    
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+   
+    
+  }
+ 
+})
+
+app.get("/feeds", async (req,res)=>{
+  try {
+    const users = await User.find({});
+
+    if(!users)  res.status(404).send("No users found")
+
+    res.status(200).send(users);
+    
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+   
+    
+  }
+
+  // const users = await User.find({});
+  // res.status(200).send(user);
+})
 
 app.use("/", (err, req, res, next) => {
   if (err) {
