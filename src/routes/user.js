c97a5aac208011eb3bb28b66e07a5da3606101a9
Rashid <rashimarakkar@gmail.com>
2024-09-22 22:59:12 +0530

    feed api done

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequests");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 const USER_SAFE_DATA = "firstName lastName age gender photoUrl";
@@ -43,6 +44,47 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.status(200).json({ message: "data fetched successfully", data });
   } catch (error) {
     res.status(500).send(error.message);
+  }
+});
+
+userRouter.get("/feeds", userAuth, async (req, res) => {
+  try {
+    const DEFAULT_LIMIT = 2;
+    const MAX_LIMIT = 2;
+    
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || DEFAULT_LIMIT;
+
+    limit = limit > MAX_LIMIT ? MAX_LIMIT : limit;
+
+    const skip = (page - 1) * limit;
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId);
+      hideUsersFromFeed.add(req.toUserId);
+    });
+
+    // Find users not in the hidden list and not the logged-in user
+    const users = await User.find({
+      _id: { $nin: [...hideUsersFromFeed, loggedInUser._id.toString()] },
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+       // Optional: Include pagination metadata
+       res.status(200).json({
+        currentPage: page,
+        usersPerPage: limit,
+        users,
+      });
+  } catch (error) {
+    res.status(400).send(error.message);
   }
 });
 
